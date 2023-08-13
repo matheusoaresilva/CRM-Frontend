4470acf23444788d7243315bccac590ba2f8d32d
Matheus Soares <matheusmatias2622@gmail.com>
2023-08-13 18:28:00 -0300

    feat: add api via cep

--- src/app/components/forms-add-customer-address/forms-add-customer-address.component.ts
+++ src/app/components/forms-add-customer-address/forms-add-customer-address.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiCepService } from 'src/app/core/services/api-cep.service';
 
 @Component({
   selector: 'app-forms-add-customer-address',
@@ -7,4 +10,29 @@ import { Component } from '@angular/core';
 })
 export class FormsAddCustomerAddressComponent {
 
+  constructor(
+    private router: Router,
+    private getCepService: ApiCepService) { }
+
+  getCEP(e: any, f: NgForm) {
+    const cep = e.target.value;
+    if(cep !== ''){
+      this.getCepService.getCep(cep).subscribe(res =>
+      {
+        console.log(res);
+        this.completeAddress(res, f);
+      });
+    }
+  }
+  completeAddress(data: any, form: NgForm){
+    form.form.patchValue({
+      street: data.logradouro,
+      number: data.complemento,
+      neighborhood: data.bairro,
+      city: data.localidade,
+      state: data.uf
+    })
+  }
 }
+
+
